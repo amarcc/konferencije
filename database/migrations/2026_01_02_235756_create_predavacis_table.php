3ac34bb8d90bbadd0b834c5970573b78ea6646ec
amarhunter01 <amarhunter01@gmail.com>
2026-01-10 12:22:03 +0100

    m6

--- database/migrations/2026_01_02_235756_create_predavacis_table.php
+++ database/migrations/2026_01_02_235756_create_predavacis_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('predavaci', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id') -> constrained() -> cascadeOnDelete();
-            $table->foreignId('konferencija_id') -> constrained('konferencija') -> cascadeOnDelete();
+            $table->foreignId('konferencija_id') -> constrained('Konferencija') -> cascadeOnDelete();
             $table->timestamps();
         });
     }
--- database/migrations/2026_01_07_182133_create_prijavas_table.php
+++ database/migrations/2026_01_07_182133_create_prijavas_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('prijava', function (Blueprint $table) {
             $table->id();   
             $table->foreignId('user_id') -> constrained() -> cascadeOnDelete();
-            $table->foreignId('konferencija_id') -> constrained('konferencija') -> cascadeOnDelete();
+            $table->foreignId('konferencija_id') -> constrained('Konferencija') -> cascadeOnDelete();
             $table->timestamps();
         });
     }
