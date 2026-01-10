23f625fd8790609f305b040ed670b0d55cc78c32
amarhunter01 <amarhunter01@gmail.com>
2026-01-10 12:08:53 +0100

    migration2

--- database/migrations/2026_01_02_233852_create_konferencijas_table.php
+++ database/migrations/2026_01_02_233852_create_konferencijas_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('konferencija', function (Blueprint $table) {
+        Schema::create('Konferencija', function (Blueprint $table) {
             $table->id();
             $table->string('ime');
             $table->foreignId('kreator') -> constrained('users') -> cascadeOnDelete();
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('konferencija');
+        Schema::dropIfExists('Konferencija');
     }
 };
