4635144653085a330ad132d640e4bad34a1dffed
amarhunter01 <amarhunter01@gmail.com>
2026-01-10 12:10:42 +0100

    migration4

--- database/migrations/2026_01_03_144909_add_pocetak_to_konferencija.php
+++ database/migrations/2026_01_03_144909_add_pocetak_to_konferencija.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('Konferencija', function (Blueprint $table) {
+        Schema::table('konferencija', function (Blueprint $table) {
             $table->timestamp('pocetak');
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('Konferencija', function (Blueprint $table) {
+        Schema::table('konferencija', function (Blueprint $table) {
             $table->dropColumn('pocetak');
         });
     }
